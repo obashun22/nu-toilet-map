7360b7f86d38083fd3ecec139aeb08e483078a5f
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-05-06 15:14:14 +0900

    Update InfoWindow作成

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,5 +1,5 @@
 import EditIcon from "@mui/icons-material/Edit";
-import { SpeedDial } from "@mui/material";
+import { ImageList, ImageListItem, SpeedDial } from "@mui/material";
 import {
   GoogleMap,
   InfoWindow,
@@ -7,6 +7,7 @@ import {
   MarkerF,
 } from "@react-google-maps/api";
 import { useState } from "react";
+import PlaceRoundedIcon from "@mui/icons-material/PlaceRounded";
 
 export const Map = () => {
   const TopNavBarHeight = 56;
@@ -19,20 +20,6 @@ export const Map = () => {
     lat: 35.1540423,
     lng: 136.9660006,
   };
-  const center = {
-    lat: 35.69575,
-    lng: 139.77521,
-  };
-
-  const positionAkiba = {
-    lat: 35.69731,
-    lng: 139.7747,
-  };
-
-  const positionIwamotocho = {
-    lat: 35.69397,
-    lng: 139.7762,
-  };
 
   const divStyle = {
     background: "white",
@@ -41,40 +28,89 @@ export const Map = () => {
 
   const [open, setOpen] = useState(false);
 
+  const options = {
+    styles: [
+      {
+        featureType: "transit",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+    ],
+  };
   return (
     <>
       <GoogleMap
         mapContainerStyle={mapStyle}
-        center={center}
+        center={nu}
         zoom={17}
         clickableIcons={false}
         options={{
           disableDefaultUI: true,
+          ...options,
         }}
         onClick={() => setOpen(false)}
       >
         <MarkerF
-          position={center}
+          position={nu}
           animation={google.maps.Animation.DROP}
           onClick={() => setOpen(true)}
         >
           {open && (
-            <InfoWindowF position={center} onCloseClick={() => setOpen(false)}>
-              <div>This is an InfoWindow</div>
+            <InfoWindowF position={nu} onCloseClick={() => setOpen(false)}>
+              <div
+                style={{
+                  width: 250,
+                  height: 350,
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    gap: 4,
+                    height: 110,
+                    overflowX: "scroll",
+                    scrollSnapType: "x mandatory",
+                  }}
+                >
+                  {itemData.map((item, index) => (
+                    <img
+                      key={index}
+                      src={item.img}
+                      alt={item.title}
+                      height={110}
+                      width={110}
+                      style={{
+                        borderRadius: 3,
+                        scrollSnapAlign: "start",
+                        scrollSnapStop: "always",
+                        objectFit: "cover",
+                      }}
+                    />
+                  ))}
+                </div>
+                <h3 style={{ margin: "10px 0px 10px", lineHeight: "120%" }}>
+                  <PlaceRoundedIcon
+                    fontSize="small"
+                    sx={{ verticalAlign: "-4px" }}
+                  />
+                  IB館南棟1階正面玄関トイレ
+                </h3>
+              </div>
             </InfoWindowF>
           )}
         </MarkerF>
-        <InfoWindowF position={positionAkiba}>
-          <div style={divStyle}>
-            <h1>秋葉原オフィス</h1>
-            どゆことですか
-          </div>
-        </InfoWindowF>
-        <InfoWindow position={positionIwamotocho}>
-          <div style={divStyle}>
-            <h1>岩本町オフィス</h1>
-          </div>
-        </InfoWindow>
       </GoogleMap>
       <SpeedDial
         ariaLabel="Post Action"
@@ -85,3 +121,77 @@ export const Map = () => {
     </>
   );
 };
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    author: "@bkristastucchio",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+    author: "@rollelflex_graphy726",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+    author: "@helloimnik",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    author: "@nolanissac",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    author: "@hjrc33",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+    author: "@tjdragotta",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+    author: "@katie_wasserman",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    author: "@silverdalex",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+    author: "@shelleypauls",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+    author: "@peterlaster",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    author: "@southside_customs",
+    cols: 2,
+  },
+];
